af8bc114b40b50ab3c93a9255fac004c65678569
user <user@KF-LAPTOP>
2022-12-11 20:17:57 +0000

    Add project files.

---
+++ sqlapp2/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlapp.Models;
+using sqlapp.Services;
+
+namespace sqlapp2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> Products;
+
+        public void OnGet()
+        {
+            ProductServices productService = new ProductServices();
+            Products = productService.GetProducts();
+        }
+    }
+}
---
+++ sqlapp2/Services/ProductServices.cs
@@ -0,0 +1,50 @@
+﻿using sqlapp.Models;
+using System.Data.SqlClient;
+
+namespace sqlapp.Services
+{
+    public class ProductServices
+    {
+        private static string db_source = "appserverken.database.windows.net";
+        private static string db_user = "sqladmin";
+        private static string db_password = "P@ssw0rd";
+        private static string db_database = "appdb";
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+            List<Product> _product_lst = new List<Product>();
+            String statement = "SELECT ProductID,ProductName,Quantity FROM Products";
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+            using(SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    Product product = new Product
+                    {
+                        ProductID = reader.GetInt32(0),
+                        ProductName = reader.GetString(1),
+                        Quantity = reader.GetInt32(2)
+                    };
+
+                    _product_lst.Add(product);
+                }
+            }
+
+            return _product_lst;
+        }
+    }
+}
